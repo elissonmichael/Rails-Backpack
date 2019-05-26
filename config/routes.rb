a8e95b49d4d871fc792010f4153960668e8e198b
Élisson Michael <elissonmichael@yahoo.com.br>
2019-05-26 18:43:54 -0300

    Add Routes Configurations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :backpacks do
+    resources :slots, except: :index
+  end
+  resources :items
+  root 'backpacks#index'
 end
