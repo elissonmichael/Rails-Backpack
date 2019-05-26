9dccafec64506fc49b8a1fb6d38bc81c995702f2
Élisson Michael <elissonmichael@yahoo.com.br>
2019-05-26 13:19:08 -0300

    Add Exception Throwing to Seeds

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,4 +1,5 @@
 class Slot < ApplicationRecord
   belongs_to :backpack
   belongs_to :item
+  validates_presence_of :quantity
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Backpack.create(capacity: 10, weight_limit: 20)
-Item.create(name: 'Silver Sword', size: 2, weight: 3)
-Item.create(name: 'Big Stone', size: 5, weight: 15)
-Item.create(name: 'Bread', size: 1, weight: 1)
+Backpack.create!(capacity: 10, weight_limit: 20)
+Item.create!(name: 'Silver Sword', size: 2, weight: 3)
+Item.create!(name: 'Big Stone', size: 5, weight: 15)
+Item.create!(name: 'Bread', size: 1, weight: 1)
