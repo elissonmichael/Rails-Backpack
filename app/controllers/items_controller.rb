e79b67b01924ce78fe60c9708ed6ee6744922e50
Élisson Michael <elissonmichael@yahoo.com.br>
2019-05-27 20:58:36 -0300

    Add Item's Scaffold CRUD

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,47 @@
+class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def edit
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item, notice: 'Item was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to @item, notice: 'Item was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to items_url, notice: 'Item was successfully destroyed.'
+  end
+
+  private
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def item_params
+      params.require(:item).permit(:name, :size, :weight)
+    end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  def to_s
+    "#{name} (size: #{size} weight: #{weight})"    
+  end
 end
