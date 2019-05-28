0fa18aea579e35aac52a1d7699806b0ee56da495
Élisson Michael <elissonmichael@yahoo.com.br>
2019-05-28 13:24:25 -0300

    Add Validations to Item and Backpack

--- app/models/backpack.rb
+++ app/models/backpack.rb
@@ -1,4 +1,10 @@
 class Backpack < ApplicationRecord
   has_many :slots
   has_many :items, through: :slots
+  validates_presence_of :capacity, :weight_limit
+  validates :capacity, :weight_limit, numericality: { greater_than: 0 }
+
+  def add(item)
+
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ApplicationRecord
+  validates_presence_of :name, :size, :weight
+  validates :size, :weight, numericality: { greater_than: 0 }
+
   def to_s
-    "#{name} (size: #{size} weight: #{weight})"    
+    "#{name} (size: #{size} weight: #{weight})"
   end
 end
